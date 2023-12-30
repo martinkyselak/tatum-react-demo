db8cfcaa42b32ba758648fa75c9ce4bb8125e66c
Martin Kyselák <martin@kyselak.name>
2023-12-30 11:50:28 +0000

    feat: update application branding
    Adds ethereum icons, updates name of the application (title etc.) and does overall cleanup.

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,8 +1,8 @@
-import ethereumlogo from './ethereum-logo.svg';
+import ethereumlogo from './logo.svg';
 
 export function Header() {
   return (
-    <header className="flex flex-col items-center text-slate-50 bg-slate-700 h-40 p-5">
+    <header className="flex flex-col items-center text-slate-50 bg-slate-700 h-30 p-5">
       <img src={ethereumlogo} alt="Ethereum logo" className="w-16 h-16" />
       <h1 className="text-xl font-bold">Ethereum Wallet</h1>
     </header>
