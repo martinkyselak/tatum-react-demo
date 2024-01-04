cf94533682c0c495774347ea2eee63b204048404
Martin Kyselák <martin@kyselak.name>
2024-01-04 19:29:31 +0000

    feat: add hash of transaction and round amounts to 5
    Adds hash to the title of timestamp, rounds transaction amounts to 5 digits.

--- src/WalletFound.tsx
+++ src/WalletFound.tsx
@@ -35,7 +35,10 @@ export function WalletFound({ walletData }: Props) {
             <li key={transaction.hash} className="py-3">
               <div className="flex items-center space-x-4">
                 <div className="flex-1 min-w-0">
-                  <p className="text-sm font-medium text-gray-900 truncate">
+                  <p
+                    className="text-sm font-medium text-gray-900 truncate"
+                    title={'Hash: ' + transaction.hash}
+                  >
                     {new Date(transaction.timestamp).toLocaleString()}
                   </p>
                   <p className="text-sm text-gray-500 truncate" title="Block #">
@@ -46,7 +49,7 @@ export function WalletFound({ walletData }: Props) {
                   className="inline-flex items-center text-base font-semibold text-gray-900"
                   title={'\u039E ' + transaction.amount}
                 >
-                  {'\u039E ' + parseFloat(transaction.amount).toFixed(2)}
+                  {'\u039E ' + parseFloat(transaction.amount).toFixed(5)}
                 </div>
               </div>
             </li>
